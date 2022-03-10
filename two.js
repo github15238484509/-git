f5d645d70bfba0df5ae11e76404e76b1d4db78eb
lmg <1138075190@qq.com>
2022-03-10 11:15:43 +0800

    55

--- two.js
+++ two.js
@@ -1,1 +1,25 @@
-132
+class Test {
+    constructor() {
+        this.name = "name"
+        this.aaa = () => {
+            console.log("aaa", this);
+        }
+    }
+    func = () => {
+        console.log("func", this);
+    }
+}
+let test = new Test()
+// console.log(tset);
+// let funct = tset.func
+// funct()
+// let obj = {
+//     a: funct
+// }
+// obj.a()
+
+var obj = {
+    a: () => {
+        console.log("a", this);
+    }
+}
